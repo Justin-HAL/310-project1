e65298a0f7db8cb7a508e17ff3bcf8c3271f6a0d
Justin-HAL <100646646+Justin-HAL@users.noreply.github.com>
2024-09-20 13:59:30 -0500

    Added Place_piece function
    This function places a piece randomly on the available places on the board.

--- Project1.py
+++ Project1.py
@@ -1,5 +1,5 @@
 import threading
-
+import random
 #2d array
 Game_board = [
     [' ','|',' ','|',' '],
@@ -8,8 +8,9 @@ Game_board = [
     ['-','-','-','-','-'],
     [' ','|',' ','|',' ']
 ]
-#turn order bool?
+
 Player1_turn = True
+Game_Over = False
 #print 2d array
 def print_board():
     global Game_board
@@ -19,6 +20,20 @@ def print_board():
         print()
 #array storing avaible moves
 
+Available_moves = [(0,0),(0,2),(0,4)
+                   ,(2,0),(2,2),(2,4),
+                   (4,0),(4,2),(4,4)]
 #method for a player/s turn.
 
+def Place_piece(piece : str):
+    global Available_moves
+    global Game_board
+
+    i = Available_moves[random.randint(0,len(Available_moves)- 1)]
+
+    row = i[0]
+    column = i[1]
+      
+    Game_board[row][column] = piece
+    Available_moves.remove(i,)
 
